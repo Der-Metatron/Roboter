1ec34f9451078928ae308d17445f1321737edc61
sprotte <metatron1972@live.de>
2021-12-14 12:10:39 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,35 +1,97 @@
-let robo = {
-    x: 0,
-    y: 0,
-    heading: 'unten'
-    };
-    
-    function step() {
-    if(robo.heading == "unten") {
-       robo.y = robo.y +1; 
-    }
-    if(robo.heading == "rechts") {
-       robo.x = robo.x +1;
-    }
-    if(robo.heading == "links") {
-       robo.x == robo.x -1; 
-    }
-    if(robo.heading == "oben") {
-       robo.y = robo.y -1; 
-    }
-    }
-    
-    function rotate() {
-        if(robo.heading == "unten") {
-           robo.heading = "rechts"; 
-        }
-        if(robo.heading == "links") {
-           robo.heading = "oben"; 
-        }
-        if(robo.heading == "rechts") {
-           robo.heading = "unten"; 
-        }
-        if(robo.heading == "oben") {
-           robo.heading = "links"; 
-        }
-        }
+/*
+Eddy the robot
+Eigenschaften:
+1. position X
+2. position Y
+3. Himmelsrichtung
+*/
+let eddy = {
+   posX: 0,
+   posY: 0,
+   // Orientierung - Himmelsrichtung (Norden, Osten, Süden, Westen)
+   heading: 'EAST'
+};
+
+/*
+Funktionen des Roboters
+1. step() -> bewegt sich einen Schritt in seine Himmelsrichtung
+2. rotate() -> rotiert sich um seine eigene Achse im Uhrzeigersinn
+*/
+function step() {
+   /*
+   FALLS OSTEN, DANN X UM EINS ERHÖHEN
+   */
+   if (eddy.heading == 'EAST') {
+       eddy.posX = eddy.posX + 1;
+   }
+   /*
+    FALLS SÜDEN, DANN Y UM EINS ERHÖREN
+        */
+   if (eddy.heading == "SOUTH") {
+       eddy.posY = eddy.posY + 1;
+   }
+
+   // FALLS WESTEN, DANN X UM EINS VERRINGERN
+   if (eddy.heading == "WEST") {
+       // DARF KEINE MINUSWERTE ERREICHEN
+       if (eddy.posX == 0) {
+           alert("Du bist an eine Grenze gestoßen!")
+       } else {
+           eddy.posX = eddy.posX - 1;
+       }
+   }
+
+   //FALLS NORDEN, DANN Y UM EINS VERRINGERN
+   if (eddy.heading == "NORTH") {
+       // DARF KEINE MINUSWERTE ERREICHEN
+       if(eddy.posY == 0) {
+           alert("Du bist an eine Grenze gestoßen!")
+       } else {
+           eddy.posY = eddy.posY - 1;
+       }
+   }
+   update();
+}
+
+function rotate() {
+   // FALLS OSTEN, DANN SÜDEN
+   if (eddy.heading == "EAST") {
+       eddy.heading = "SOUTH";
+       update();
+       return;
+   };
+
+
+   // FALLS SÜDEN, DANN WESTEN
+   if (eddy.heading == "SOUTH") {
+       eddy.heading = "WEST";
+       update();
+       return;
+   }
+
+   // FALLS WESTEN, DANN NORDEN
+   if (eddy.heading == "WEST") {
+       eddy.heading = "NORTH";
+       update();
+       return;
+   }
+
+   // FALLS NORDEN, DANN OSTEN
+   if (eddy.heading == "NORTH") {
+       eddy.heading = "EAST";
+       update();
+       return;
+   }
+}
+
+function update() {
+   document.getElementById("posX").innerHTML = eddy.posX;
+   document.getElementById("posY").innerHTML = eddy.posY;
+   document.getElementById("heading").innerHTML = eddy.heading;
+}
+
+/*
+CANVAS ZEICHNEN IN HTML UND ÜBER CONTEXT IMPORTIEREN
+ROBOTER EINZEICHNEN
+IN DER CANVAS BEWEGUNGEN AUSFÜHREN
+*/
