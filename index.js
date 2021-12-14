18aa3334fa85420bf259cc8a765a62a5fc5c3520
sprotte <metatron1972@live.de>
2021-12-14 10:55:28 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,30 @@
+let robo = {
+    x: 0,
+    y: 0,
+    heading: 'unten'
+    };
+    
+    function step() {
+    if(robo.heading == "unten") {
+       robo.y = robo.y +1; 
+    }
+    if(robo.heading == "rechts") {
+       robo.x = robo.x +1;
+    }
+    if(robo.heading == "links") {
+       robo.x == robo.x +1; 
+    }
+    if(robo.heading == "oben") {
+       robo.y = robo.y +1; 
+    }
+    }
+    
+    function rotate() {
+        if(robo.heading == "unten") {
+           robo.heading = "rechts"; 
+        }
+        if(robo.heading == "links") {
+           robo.heading = "oben"; 
+        }
+    
+    }
